91c57075d0137f43aeceb03ecdc72c9ea221c712
Stijn Geysen <stijngeysen@gmail.com>
2016-03-03 00:17:43 +0100

    Comments uitgebreid en ook in UDPClient, Bug fix in server: datapakketten moeten binnen de while loop geïnitialiseerd worden want anders krijg je problemen als de vorige boodschappen langer waren: de laatste letters blijven behouden. BV: eerste message: "test1234" en tweede message: "qs" zal als 2de response geven: "qsst1234" omdat de "te" overschreven wordt door "qs" en de "st1234" gewoon blijft staan. Dit is dus opgelost door de datapacket initialisatie binnen de loop te brengen => er wordt steeds met een leeg packet gestart.
    ps: ik kan dit ook testen op mijn eigen computer, op kot.

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -9,20 +9,38 @@ class UDPClient
 	static int lengte = 512;
    public static void main(String args[]) throws Exception
    {
+       //initialize data arrays
+       byte[] receiveData = new byte[lengte];
+       byte[] sendData = new byte[lengte];
+       
+       //Make new client Socket for UDP connections
+       DatagramSocket clientSocket = new DatagramSocket();
+       //Get the UDP server address (destination address)
+       InetAddress IPAddress = InetAddress.getByName("localhost");
+       
+	  //Get input message from User
       BufferedReader inFromUser =
          new BufferedReader(new InputStreamReader(System.in));
-      DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
-      byte[] sendData = new byte[lengte];
-      byte[] receiveData = new byte[lengte];
       String sentence = inFromUser.readLine();
       sendData = sentence.getBytes();
+      
+      //Construct new DatagramPacket to send
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+      //Send the packet
       clientSocket.send(sendPacket);
+      
+      //Construct DatagramPacket for response from server
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      //Receive the response from server
       clientSocket.receive(receivePacket);
+      
+      //Process response
+      InetAddress ServerAddress = receivePacket.getAddress();
+      System.out.println("Server IP: " + ServerAddress);
       String modifiedSentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + modifiedSentence);
+      System.out.println("Message from the server:" + modifiedSentence);
+      
+      //Client is done, so close the connections
       clientSocket.close();
    }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -9,12 +9,15 @@ public class UDPServer {
 	//Main class
 	public static void main(String[] args) throws IOException {
 		DatagramSocket welcomeSocket = new DatagramSocket(port);
-        //initialize data arrays
-        byte[] receiveData = new byte[lengte];
-        byte[] sendData = new byte[lengte];
         
 		while (! welcomeSocket.isClosed())
 		{
+	        //initialize empty data arrays (if this is placed outside the while loop,
+			//the byte array will contain bytes of previous, longer messages if a short messages
+			//has to be processed
+	        byte[] receiveData = new byte[lengte];
+	        byte[] sendData = new byte[lengte];
+	        
     		//Construct new datagrampacket to receive
     		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
     		//get the next packet
@@ -24,14 +27,15 @@ public class UDPServer {
     		
     		//process data
     		String data = new String(receivePacket.getData());
-    		System.out.println("RECEIVED: " + data);
-    		System.out.println("IP:" + IPAddress);
+    		System.out.println("Client sent: " + data);
+    		System.out.println("Client IP:" + IPAddress);
     		String welcome = new String("Welcome client: ");
     		String capitalizedSentence = data.toUpperCase();
     		String toSend = welcome + capitalizedSentence;
     		sendData  = toSend.getBytes();
     		
-    		//find out to which port to send the answer
+    		//find out to which port to send the answer, that is, the port from which the client
+    		//has send his package
     		int receivePort = receivePacket.getPort();
     		//Construct sendPacket
     		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePort);
