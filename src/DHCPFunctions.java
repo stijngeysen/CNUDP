6fe4f27fb6772f941399249863a2228be0068494
marnixmoons <marnixmoons@gmail.com>
2016-03-06 14:59:44 +0100

    ack

--- src/DHCPFunctions.java
+++ src/DHCPFunctions.java
@@ -102,7 +102,33 @@ public class DHCPFunctions{
 		System.out.println("DHCPRequest message broadcasted by me (Client)");
 	}
 
-	public void DHCPAck(){
+	public void DHCPAck(DatagramSocket socket, DHCPMessage message, DatagramPacket packet, InetAddress yourIP){
+		//op:		2 (reply)
+		//htype: 	1 (ethernet)
+		//hlen:		6 (IEEE 802 MAC addresses)
+		//hops:		0
+		//xid:		vorig transactieID
+		//sec:		0
+		//flags		-32768 (2's complement decimaal voor 1000 0000 0000 0000 , de broadcast flag)
+		//CIP		0 (Client heeft nog geen IP)
+		//YI		server's IP
+		//SI		server's IP of 255.255.255.255
+		//GI		byte[4]
+		//Client Hardware Address (MAC)	bv 01:23:45:67:89:ab (16 bytes)
+		//SName		byte[64]
+		//BootFile	byte[128]
+		//Options
+		int sec = 0; //TODO: nog geen idee wat we we hier mee moeten doen --> blijft 0, wordt pas na ack gebruikt
+		byte[] CHA = message.getClientHardwareAddress();
+		
+		DHCPMessage acknowledgeMessage = new DHCPMessage(Utils.toBytes(2, 1), Utils.toBytes(1, 1), Utils.toBytes(6, 1), Utils.toBytes(0), 
+				message.getTransactionID(), Utils.toBytes(sec, 2), Utils.toBytes(-32768, 2), Utils.toBytes(0), yourIP.getAddress(), socket.getLocalAddress().getAddress(), 
+				new byte[4], CHA, new byte[64], new byte[128], DHCPMessageType.DHCPACK.getBytes());
+		if (message.getFlags()[0] == 1) { //1e bit van flags = 1 --> broadcast
+			broadcastMessage(socket, acknowledgeMessage, packet.getPort()); //normaal is 68 UDP poort voor DHCP client
+		} else { // 1e bit van flags = 0 --> unicast
+			unicastMessage(socket, acknowledgeMessage, packet.getPort(), packet.getAddress()); //normaal is 68 UDP poort voor DHCP client
+		}
 		System.out.println("DHCPAcknowledge message broadcasted by me (Server)");
 	}
 
