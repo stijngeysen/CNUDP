aaa16101676ba74b63f027ef1a76e9fc6c2f0b76
marnixmoons <marnixmoons@gmail.com>
2016-03-06 14:29:26 +0100

    enums en request

--- src/DHCPFunctions.java
+++ src/DHCPFunctions.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.util.Random;
 
 
-public class DHCPFunctions {
+public class DHCPFunctions{
 	//TODO: vragen of Random gebruikt mag worden
 	static Random rand = new Random();
 
@@ -36,13 +36,14 @@ public class DHCPFunctions {
 
 		DHCPMessage discoverMessage = new DHCPMessage(Utils.toBytes(1, 1), Utils.toBytes(1, 1), Utils.toBytes(6, 1), Utils.toBytes(0), 
 				transactionID, Utils.toBytes(sec, 2), Utils.toBytes(-32768, 2), Utils.toBytes(0), new byte[4], new byte[4], 
-				new byte[4], CHA, new byte[64], new byte[128], new byte[0]);
+				new byte[4], CHA, new byte[64], new byte[128], DHCPMessageType.DHCPDISCOVER.getBytes());
 
-		broadcastMessage(discoverMessage, 1234, socket); //67 is UDP poort voor DHCP server: Client -> server communication
+		broadcastMessage(socket, discoverMessage, 1234); //67 is UDP poort voor DHCP server: Client -> server communication
 		System.out.println("DHCPDiscover message broadcasted by me (Client)");
 	}
 
 	public static void DHCPOffer(DatagramSocket socket, DHCPMessage message, DatagramPacket packet, InetAddress yourIP) {
+		//TODO: SI en GI heb ik mijn twijfels nog over
 		//op:		2 (reply)
 		//htype: 	1 (ethernet)
 		//hlen:		6 (IEEE 802 MAC addresses)
@@ -51,28 +52,53 @@ public class DHCPFunctions {
 		//sec:		0
 		//flags		-32768 (2's complement decimaal voor 1000 0000 0000 0000 , de broadcast flag)
 		//CIP		0 (Client heeft nog geen IP)
-		//YI		byte[4]
-		//SI		byte[4] of 255.255.255.255 //TODO: moet hier die 255? --> nee, deze code wordt enkel gebruikt voor broadcast
+		//YI		server's IP
+		//SI		server's IP of 255.255.255.255 //TODO: moet hier die 255? --> nee, deze code wordt enkel gebruikt voor broadcast
 		//GI		byte[4] (niet gebruikt door clients) //TODO: wel voor servers??? --> hangt volgens mij van de verbinding af, dus wel als er sprake is van een router bv
 		//Client Hardware Address (MAC)	bv 01:23:45:67:89:ab (16 bytes)
 		//SName		byte[64]
 		//BootFile	byte[128]
 		//Options
-		int sec = 0; //TODO: nog geen idee wat we we hier mee moeten doen --> volgens wikipedia blijft er 0 staan wat ik wel raar vind..
-		byte[] CHA = new byte[16]; //TODO: wat moet hier?
+		int sec = 0; //TODO: nog geen idee wat we we hier mee moeten doen --> blijft 0, wordt pas na ack gebruikt
+		byte[] CHA = message.getClientHardwareAddress();
 		
 		DHCPMessage offerMessage = new DHCPMessage(Utils.toBytes(2, 1), Utils.toBytes(1, 1), Utils.toBytes(6, 1), Utils.toBytes(0), 
 				message.getTransactionID(), Utils.toBytes(sec, 2), Utils.toBytes(-32768, 2), Utils.toBytes(0), yourIP.getAddress(), socket.getLocalAddress().getAddress(), 
-				new byte[4], CHA, new byte[64], new byte[128], new byte[0]);
+				new byte[4], CHA, new byte[64], new byte[128], DHCPMessageType.DHCPOFFER.getBytes());
 		if (message.getFlags()[0] == 1) { //1e bit van flags = 1 --> broadcast
-			broadcastMessage(offerMessage, packet.getPort(), socket); //normaal is 68 UDP poort voor DHCP client
+			broadcastMessage(socket, offerMessage, packet.getPort()); //normaal is 68 UDP poort voor DHCP client
 		} else { // 1e bit van flags = 0 --> unicast
-			sendMessage(offerMessage, packet.getPort(), socket, packet); //normaal is 68 UDP poort voor DHCP client
+			unicastMessage(socket, offerMessage, packet.getPort(), packet.getAddress()); //normaal is 68 UDP poort voor DHCP client
 		}
 		System.out.println("DHCPOffer message broadcasted by me (Server)");
 	}
 
-	public void DHCPRequest() {
+	public void DHCPRequest(DatagramSocket socket, DHCPMessage message, DatagramPacket packet) {
+		//op:		1 (request) (1 = bootrequest, 2 = bootreply)
+		//htype: 	1 (ethernet) (hardware address type)
+		//hlen:		6 (IEEE 802 MAC addresses) (hardware address length)
+		//hops:		0 (optionnaly used by relay agents)
+		//xid:		vorig transactieID
+		//sec:		0 (seconds elapsed since client began address acquisition or renewal process)
+		//flags		0x8000 (broadcast) of 0x0000 (unicast)
+		//CIP		0 (Client heeft nog geen IP)
+		//YI		byte[4] (your IP-address)
+		//SI		unicastaddress naar server
+		//GI		byte[4] (niet gebruikt door clients) (relay agent IP address)
+		//Client Hardware Address (MAC)	bv 01:23:45:67:89:ab (16 bytes)
+		//SName		byte[64] (optional server name)
+		//BootFile	byte[128]
+		//Options	var
+		int sec = 0;
+		DHCPMessage requestMessage = new DHCPMessage(Utils.toBytes(1, 1), Utils.toBytes(1, 1), Utils.toBytes(6, 1), Utils.toBytes(0), 
+				message.getTransactionID(), Utils.toBytes(sec, 2), Utils.toBytes(0, 2), Utils.toBytes(0), new byte[4], message.getServerIP(), 
+				new byte[4], message.getClientHardwareAddress(), message.getServerHostName(), new byte[128], DHCPMessageType.DHCPREQUEST.getBytes());
+
+		if (message.getFlags()[0] == 1) {
+			broadcastMessage(socket, requestMessage, packet.getPort());
+		} else {
+			unicastMessage(socket, requestMessage, packet.getPort(), packet.getAddress());
+		}
 		System.out.println("DHCPRequest message broadcasted by me (Client)");
 	}
 
@@ -88,7 +114,7 @@ public class DHCPFunctions {
 		System.out.println("DHCPRelease message unicasted by me (Client)");
 	}
 
-	public static void broadcastMessage(DHCPMessage message, int deliveryPort, DatagramSocket socket){
+	public static void broadcastMessage(DatagramSocket socket, DHCPMessage message, int deliveryPort){
 		try {
 			byte[] msg = message.makeMessage();
 			InetAddress broadcast = InetAddress.getByName("10.33.14.246");
@@ -99,10 +125,10 @@ public class DHCPFunctions {
 		}
 	}
 	
-	public static void sendMessage(DHCPMessage message, int port, DatagramSocket socket, DatagramPacket packet) {
+	public static void unicastMessage(DatagramSocket socket, DHCPMessage message, int port, InetAddress address) {
 		try {
 			byte[] msg = message.makeMessage();
-			DatagramPacket sendPacket = new DatagramPacket(msg, msg.length, packet.getAddress(), packet.getPort());
+			DatagramPacket sendPacket = new DatagramPacket(msg, msg.length, address, port);
 			socket.send(sendPacket);
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/DHCPMessageType.java
+++ src/DHCPMessageType.java
@@ -1,12 +1,33 @@
 
 public enum DHCPMessageType 
-{   DHCPDISCOVER,	//a client broadcasts to locate servers
-	DHCPOFFER,	    //a server offers an IP address to the device
+{   DHCPDISCOVER,		//a client broadcasts to locate servers
+	DHCPOFFER,	    	//a server offers an IP address to the device
 	DHCPREQUEST,		//client accepts offers from DHCP server
 	DHCPACK,			//server to client + committed IP address
 	DHCPNAK,			//server to client to state net address incorrect
 	DHCPRELEASE;		//graceful shutdown from client to Server
+	
+	public DHCPMessageType type;
 
-	private DHCPMessageType() {
+	public byte[] getBytes() {
+		switch (this.type) {
+			case DHCPDISCOVER: return Utils.toBytes(0, 3);
+			case DHCPOFFER: return Utils.toBytes(1, 3);
+			case DHCPREQUEST: return Utils.toBytes(2, 3);
+			case DHCPACK: return Utils.toBytes(3, 3);
+			case DHCPNAK: return Utils.toBytes(4, 3);
+			default: return Utils.toBytes(5, 3);
+		}
+	}
+	
+	public static DHCPMessageType getType(byte[] bytes) {
+		switch (Utils.fromBytes(bytes)) {
+			case 0: return DHCPDISCOVER;
+			case 1: return DHCPOFFER;
+			case 2: return DHCPREQUEST;
+			case 3: return DHCPACK;
+			case 4: return DHCPNAK;
+			default: return DHCPRELEASE;
+		}
 	}
 }
