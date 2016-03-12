b38a17b0ad6a649c3991ef610f33c55962faf96d
marnixmoons <marnixmoons@gmail.com>
2016-03-12 16:08:49 +0100

    client

--- src/DHCPClient.java
+++ src/DHCPClient.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 class DHCPClient
 {
@@ -13,8 +14,6 @@ class DHCPClient
 		DatagramSocket socket = new DatagramSocket(port);
 		while(! socket.isClosed()){
 			DHCPFunctions.DHCPDiscover(socket);
-
-			System.out.println();
 			
 			//initialize empty data arrays (if this is placed outside the while loop,
 			//the byte array will contain bytes of previous, longer messages if a short messages
@@ -28,50 +27,93 @@ class DHCPClient
 			//process data
 			byte[] msg = receivePacket.getData();
 			DHCPMessage message = new DHCPMessage(msg);
-			System.out.println("Message received! ");
-			message.getMessageType();
+			if (message.getMessageOption(53) != DHCPMessageType.DHCPOFFER.getBytes()) {
+				System.out.println("ERROR: No DHCPOffer received.");
+				continue;
+			}
 			
 			//Data information
+			System.out.println();
+			System.out.println("DATA INFORMATION");
 			System.out.println("Transaction ID: " + Utils.fromBytes(message.getTransactionID()));
 			System.out.println("Hardware Address Length: " + Utils.fromBytes(message.getHardwareAddressLength()));
 			System.out.println("Client IP: " + InetAddress.getByAddress(message.getClientIP()));
 			System.out.println("Your IP: " + InetAddress.getByAddress(message.getYourIP()));
 			System.out.println("Server IP: " + InetAddress.getByAddress(message.getServerIP()));
-			
 			System.out.println();
 			
-			DHCPFunctions.DHCPRequest(socket, message, receivePacket);
-			
-			//initialize empty data arrays (if this is placed outside the while loop,
-			//the byte array will contain bytes of previous, longer messages if a short messages
-			//has to be processed
-			byte[] requestData = new byte[lengte];
-
-			DatagramPacket requestPacket = new DatagramPacket(requestData, lengte);
-			//Receive the response from server
-			socket.receive(requestPacket);
-			
-			//process data
-			byte[] msg2 = requestPacket.getData();
-			DHCPMessage message2 = new DHCPMessage(msg2);
-			System.out.println("Message received! ");
-			message2.getMessageType();
-			
-			//Data information
-			System.out.println("Transaction ID: " + Utils.fromBytes(message2.getTransactionID()));
-			System.out.println("Hardware Address Length: " + Utils.fromBytes(message2.getHardwareAddressLength()));
-			System.out.println("Client IP: " + InetAddress.getByAddress(message2.getClientIP()));
-			System.out.println("Your IP: " + InetAddress.getByAddress(message2.getYourIP()));
-			System.out.println("Server IP: " + InetAddress.getByAddress(message2.getServerIP()));
-			System.out.println("Seconds:  " + Utils.fromBytes(message2.getNumberOfSeconds()));
-			
-			System.out.println();
-			
-			//Close socket
-			if(! Arrays.equals(msg2, new byte[0])){
-				System.out.println("Socket Closing");
-				socket.close();
+			//Requesting
+			while (true) {
+				DHCPFunctions.DHCPRequest(socket, message, receivePacket);
+				
+				//initialize empty data arrays (if this is placed outside the while loop,
+				//the byte array will contain bytes of previous, longer messages if a short messages
+				//has to be processed
+				byte[] requestData = new byte[lengte];
+	
+				DatagramPacket requestPacket = new DatagramPacket(requestData, lengte);
+				//Receive the response from server
+				socket.receive(requestPacket);
+				
+				//process data
+				byte[] msg2 = requestPacket.getData();
+				DHCPMessage message2 = new DHCPMessage(msg2);
+				if (message2.getMessageOption(53) != DHCPMessageType.DHCPACK.getBytes()) {
+					System.out.println("ERROR: Negative acknowledge received.");
+					continue;
+				}
+				
+				//Data information
+				System.out.println();
+				System.out.println("DATA INFORMATION");
+				System.out.println("Transaction ID: " + Utils.fromBytes(message2.getTransactionID()));
+				System.out.println("Hardware Address Length: " + Utils.fromBytes(message2.getHardwareAddressLength()));
+				System.out.println("Client IP: " + InetAddress.getByAddress(message2.getClientIP()));
+				System.out.println("Your IP: " + InetAddress.getByAddress(message2.getYourIP()));
+				System.out.println("Server IP: " + InetAddress.getByAddress(message2.getServerIP()));
+				System.out.println("Seconds:  " + Utils.fromBytes(message2.getNumberOfSeconds()));
+				System.out.println();
+				
+				break;
 			}
+			
+			//Extended requesting
+			//Requesting
+			while (true) {
+				int IPLeaseTime = Utils.fromBytes(message.getMessageOption(51));
+				TimeUnit.SECONDS.sleep(IPLeaseTime/2);
+				
+				DHCPFunctions.DHCPExtendedRequest(socket, message, receivePacket);
+				
+				//initialize empty data arrays (if this is placed outside the while loop,
+				//the byte array will contain bytes of previous, longer messages if a short messages
+				//has to be processed
+				byte[] extendedRequestData = new byte[lengte];
+	
+				DatagramPacket extendedRequestPacket = new DatagramPacket(extendedRequestData, lengte);
+				//Receive the response from server
+				socket.receive(extendedRequestPacket);
+				
+				//process data
+				msg = extendedRequestPacket.getData();
+				message = new DHCPMessage(msg);
+				if (message.getMessageOption(53) != DHCPMessageType.DHCPACK.getBytes()) {
+					System.out.println("ERROR: Negative acknowledge received.");
+					break;
+				}
+				
+				continue;
+			}
+			
+			DHCPFunctions.DHCPRelease();
+			
+			
+			
+//			//Close socket
+//			if(! Arrays.equals(msg2, new byte[0])){
+//				System.out.println("Socket Closing");
+//				socket.close();
+//			}
 			
 		}
 	}
